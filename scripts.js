5bedaf50ec046c7952cb14fb8c5be647bf5aec08
Joshua Alletto <joshalletto@gmail.com>
2017-12-12 15:23:31 -0600

    can now display dots and dashes on the screen.

--- scripts.js
+++ scripts.js
@@ -1,26 +1,53 @@
-let light = document.querySelector('.flash')
-
-setInterval(function (){
-  if(light.style.visibility === "visible"){
-    light.style.visibility = "hidden"
-  } else {
-    light.style.visibility = "visible"
-  }
-}, 400)
-
-
-
-
-
-let dot = 1
+let dot = 1000
 let dash = dot * 3
-let symbolSpace = dot
+let space = dot
 let letterSpace = dash
 let wordSpace = dot * 7
 
-const MorseCode = {
-  a:
+const morseCode = {
+  a: [dot,dash],
+  b: [dash,dot,dot, dot],
 }
+const light = document.querySelector('.flash')
+
+function lightOn(){
+ light.style.display = "block"
+}
+
+function lightOff(){
+  light.style.display = "none"
+}
+
+// function showSymbol(symbol){
+//   lightOn()
+//   setTimeout(lightOff,symbol)
+// }
+
+
+let a = [dot, space, dash, space, dot]
+
+function myLoop (array,i) {
+  setTimeout(function () {
+    if(light.style.display === "none"){
+      lightOn()
+    }
+    else{
+      lightOff()
+    }
+    i++;
+    if (i <= array.length - 1) {
+        myLoop(array, i);
+    }
+  }, array[i])
+}
+
+
+
+
+
+myLoop(a,0)
+// flash for one second
+// flash for 3 seconds
 // The length of a dot is 1 time unit
 // A dash is 3 time units
 // The space between symbols (dots and dashes) of the same letter is 1 time unit
